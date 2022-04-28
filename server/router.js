aee5dad9d40eb5896a0e6b70be4262774484ba56
Christian Eliopoulos <cge7497@rit.edu>
2022-04-27 22:17:36 -0400

    Deletes unused player.js model and controller.

--- server/router.js
+++ server/router.js
@@ -3,7 +3,6 @@ const mid = require('./middleware');
 
 const router = (app) => {
   app.get('/getToken', mid.requiresSecure, controllers.Account.getToken);
-  // app.get('/getDomos', mid.requiresLogin, controllers.Player.getPlayers);
 
   app.route('/getLevel')
     .get(mid.requiresSecure, controllers.Game.getLevel)
